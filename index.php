b7de4432be6735066225c0e55e25ccb5edce6910
reyhanboim <54893437+reyhanboim@users.noreply.github.com>
2021-07-19 00:22:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ use PEAR2\Net\RouterOS;
 require_once 'PEAR2/Autoload.php';
 header('Content-Type: text/plain');
 
-$util=new RouterOS\Util($client=new RouterOS\Client('172.25.25.34', 'reyhan', 'reyhan1010'));
+$util=new RouterOS\Util($client=new RouterOS\Client('xxx.xxx.xxx.xxx', 'username_userman', 'password_userman'));
 $client->sendSync($util->setMenu('/tool user-manager user')->newRequest('add', [
 'username' => 'reyhan',
 'password' => '123123',
